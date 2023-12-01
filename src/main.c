f263d6c36d79f4a04a1c4b9234e5a06b98f8249f
barthr <info@bartfokker.com>
2023-12-02 00:28:18 +0100

    Remove comments

--- src/main.c
+++ src/main.c
@@ -2,10 +2,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	// be_node *node  = parse_be_stream("4:spam");
-	// be_node *p_int = parse_be_stream("i3e");
-	//
-	char *input = "li-3ell4:spami3eeee";
+	char *input = argv[1];
 	printf("INPUT: %s\n", input);
 	be_node *n_int = parse_be_stream(input);
 
@@ -15,12 +12,5 @@ int main(int argc, char **argv) {
 	}
 
 	pretty_print_tree(n_int);
-	// be_node nested_nested_node = ((be_node *)nested_node.data)[0];
-	// be_node nested_nested_nested_node = ((be_node
-	// *)nested_nested_node.data)[0];
-
-	// printf("node is of type %d and data is %s\n", node->type, (char
-	// *)node->data); printf("p_int is of type %d and data is %d\n",
-	// p_int->type, *(int *)p_int->data);
 	return 0;
 }
